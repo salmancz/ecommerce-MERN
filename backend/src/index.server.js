664115c4af80ec30afb3cec904de0679ac26a75e
salmancz <salman.jsdev@gmail.com>
2022-10-06 11:28:55 +0530

    modified:   backend/.env 	modified:   backend/package.json 	modified:   backend/src/index.server.js 	new file:   backend/src/models/user.js 	new file:   backend/src/routes/user.js

--- backend/src/index.server.js
+++ backend/src/index.server.js
@@ -1,13 +1,27 @@
-// const express = require("express");
-// const env = require("dotenv");
+const express = require("express");
 require("dotenv").config();
-// const app = express();
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-console.log(process.env.PORT);
+//routes
+const userRoutes = require("./routes/user");
 
-//environment variable or constants
-// env.config();
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWD}@cluster0.drr5v.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("connected to mongodb");
+  });
 
-// app.listen(process.env.PORT, () => {
-//   console.log("Server is running in a port: " + process.env.PORT);
-// });
+app.use(bodyParser());
+app.use("/api", userRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running in a port: " + process.env.PORT);
+});
---
+++ backend/src/routes/user.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/signup", (req, res) => {}); // end of router.post
+
+router.post("/signin", (req, res) => {}); // end of router.post
+
+module.exports = router;
