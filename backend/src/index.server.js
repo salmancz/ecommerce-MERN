b15f12355773e487e3da155a6bf45e00a4f11b60
salmancz <salman.jsdev@gmail.com>
2022-10-06 09:37:49 +0530

    new file:   backend/.env.development 	new file:   backend/package-lock.json 	modified:   backend/package.json 	new file:   backend/src/index.server.js

---
+++ backend/src/index.server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+// const env = require("dotenv");
+require("dotenv").config();
+const app = express();
+
+console.log(process.env.PORT);
+
+//environment variable or constants
+// env.config();
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running in a port: " + process.env.PORT);
+});
