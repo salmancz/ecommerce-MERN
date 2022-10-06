73cbcd8cf1414e668160c1b1001ab491f996d9f0
salmancz <salman.jsdev@gmail.com>
2022-10-06 09:51:05 +0530

    renamed:    backend/.env.development -> backend/.env 	modified:   backend/src/index.server.js

--- backend/src/index.server.js
+++ backend/src/index.server.js
@@ -1,13 +1,13 @@
-const express = require("express");
+// const express = require("express");
 // const env = require("dotenv");
 require("dotenv").config();
-const app = express();
+// const app = express();
 
 console.log(process.env.PORT);
 
 //environment variable or constants
 // env.config();
 
-app.listen(process.env.PORT, () => {
-  console.log("Server is running in a port: " + process.env.PORT);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log("Server is running in a port: " + process.env.PORT);
+// });
